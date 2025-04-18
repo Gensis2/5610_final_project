9fea8cb7d58727157e850e685f857781e2ce436a
Brandon Nguyen <brandonlnguyen0812@gmail.com>
2025-04-18 15:23:41 -0400

    merging accuracy select into generate_graphs.py

--- data_processing/generate_graphs.py
+++ data_processing/generate_graphs.py
@@ -6,6 +6,20 @@ import argparse
 def load_data(file_path):
     return pd.read_csv(file_path)
 
+def accuracy_select(df, label):
+    acc_row = df
+    highest_accuracy_row = acc_row.loc[acc_row['testing_acc'].idxmax()]
+    lowest_loss_row = acc_row.loc[acc_row['training_loss'].idxmin()]
+    columns_to_match = [col for col in acc_row.columns if col not in [label, 'testing_acc', 'training_loss']]
+    acc_df = acc_row[
+        acc_row[columns_to_match].eq(highest_accuracy_row[columns_to_match]).all(axis=1)
+    ]
+    loss_df = acc_row[
+        acc_row[columns_to_match].eq(lowest_loss_row[columns_to_match]).all(axis=1)
+    ]
+
+    return acc_df, loss_df
+
 # Find best lr and epoch while varying batch size
 def find_best_lr_and_epoch(data):
     # Implement logic to find the best learning rate and epoch based on the data
