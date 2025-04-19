d95213ceb58aefab8c9224ac90a1882304d2984c
Brandon Nguyen <brandonlnguyen0812@gmail.com>
2025-04-19 16:10:57 -0400

    adding argument parser to run.py and adding README.md for code run instructions

---
+++ README.md
@@ -0,0 +1,23 @@
+# Instructions for running code
+Open terminal in root directory of the project
+
+If doing single CNN run, run the following line:
+`python run.py --model=cnn`
+
+If doing case study CNN run, run the following line:
+`python run.py --model=cnn --case_study`
+
+If doing single SNN run, run the following line:
+`python run.py --model=snn`
+
+If doing case study CNN run, run the following line:
+`python run.py --model=snn --case_study`
+
+# Required Packages:
+pytorch
+numpy
+pandas
+mnist_data
+
+
+
--- run.py
+++ run.py
@@ -1,7 +1,28 @@
 import cnn_run
 import snn_run
+import argparse
 
-#cnn_run.case_study()
-snn_run.case_study()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run CNN or SNN model")
+    parser.add_argument("--model", type=str, choices=["cnn", "snn"], required=True, help="Model type to run")
+    parser.add_argument("--case_study", action="store_true", help="Run case study")
 
-#snn_run.single_run()
+    args = parser.parse_args()
+
+    print(args.case_study)
+
+    if args.case_study:
+        print("Running case study...")
+    else:
+        print("Running single run...")
+
+    if args.model == "cnn" and not args.case_study:
+        cnn_run.cnn_run()
+    elif args.model == "snn" and not args.case_study:
+        snn_run.single_run()
+    elif args.model == "cnn" and args.case_study:
+        cnn_run.case_study()
+    elif args.model == "snn" and args.case_study:
+        snn_run.case_study()
+    else:
+        print("Invalid arguments. Please specify a valid model and options.")
